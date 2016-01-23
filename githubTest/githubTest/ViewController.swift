228151477f00c07ed6172bb4db9ad164de75bf40
reznov <baogu8760@gmail.com>
2016-01-23 17:59:52 +0800

    测试提交

---
+++ githubTest/githubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubTest
+//
+//  Created by reznov on 16/1/23.
+//  Copyright © 2016年 reznov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
